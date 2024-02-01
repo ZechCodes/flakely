dc7f85bcfec34560273e3eba9b8b5e6949e1edc5
Zech Zimmerman <hi@zech.codes>
2024-01-31 19:17:33 -0500

    Tweak the implementation

--- flakely.py
+++ flakely.py
@@ -19,7 +19,6 @@ def _use_if_set_else(value: T, default: Callable[[], T]) -> T:
 class Flakely:
     COUNTER_SIZE = 2
     DEVICE_ID_SIZE = 4
-    IDENTIFIER_SIZE = 4
     PROCESS_ID_SIZE = 4
     SIGNATURE_SIZE = 32
     TICK_SIZE = 4
@@ -38,15 +37,14 @@ class Flakely:
         self._last_generated = 0
         self._generation_counter = 0
 
-    def generate(self, identifier: int) -> int:
-        return int.from_bytes(self.generate_bytes(identifier), byteorder=self.BYTE_ORDER)
+    def generate(self) -> int:
+        return int.from_bytes(self.generate_bytes(), byteorder=self.BYTE_ORDER)
 
-    def generate_bytes(self, identifier: int) -> bytes | bytearray:
+    def generate_bytes(self) -> bytes | bytearray:
         tick = self.get_tick()
-        self.update_counter(tick)
+        self._update_counter(tick)
 
         flake = bytearray()
-        flake.extend(identifier.to_bytes(self.IDENTIFIER_SIZE, self.BYTE_ORDER))
         flake.extend(self._generation_counter.to_bytes(self.COUNTER_SIZE, self.BYTE_ORDER))
         flake.extend(self.process.to_bytes(self.PROCESS_ID_SIZE, self.BYTE_ORDER))
         flake.extend(self.device.to_bytes(self.DEVICE_ID_SIZE, self.BYTE_ORDER))
@@ -60,17 +58,20 @@ class Flakely:
     def get_tick(self) -> int:
         return int(time.time())
 
-    def update_counter(self, tick: int):
+    def validate(self, snowflake: int | bytes | bytearray) -> bool:
+        payload_size = self.COUNTER_SIZE + self.DEVICE_ID_SIZE + self.PROCESS_ID_SIZE + self.TICK_SIZE
+        if isinstance(snowflake, int):
+            flake = snowflake.to_bytes(payload_size + self.SIGNATURE_SIZE, self.BYTE_ORDER)
+        else:
+            flake = snowflake
+
+        payload, signature = flake[:payload_size], flake[payload_size:]
+        return signature == self.get_signature(payload)
+
+    def _update_counter(self, tick: int):
         if tick == self._last_generated:
             self._generation_counter += 1
 
         else:
             self._last_generated = tick
             self._generation_counter = 0
-
-    def validate(self, flake: int) -> bool:
-        payload_size = self.COUNTER_SIZE + self.DEVICE_ID_SIZE + self.IDENTIFIER_SIZE + self.PROCESS_ID_SIZE + self.TICK_SIZE
-        _flake = flake.to_bytes(payload_size + self.SIGNATURE_SIZE, self.BYTE_ORDER)
-        payload = _flake[:payload_size]
-        signature = _flake[payload_size:]
-        return signature == self.get_signature(payload)
